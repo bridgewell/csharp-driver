641de6055ed58d296348d9abb5ff53b08a8de49b
yahsuan <yahsuan@bridgewell.com>
2016-08-18 17:22:19 +0800

    chagne assembly name

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -13,7 +13,7 @@
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Reflection.AssemblyCopyright("Copyright © 2016 by DataStax")]
 [assembly: System.Reflection.AssemblyVersion("3.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("3.0.8 (git 070343b)")]
+[assembly: System.Reflection.AssemblyInformationalVersion("3.0.8 (git 0721387)")]
 [assembly: System.Reflection.AssemblyFileVersion("3.0.8.308")]
 
 
